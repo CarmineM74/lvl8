a0e3f512e5a478f398bb1d006b376f0d972c9506
Carmine Moleti <carmine@moleti.it>
2024-10-17 17:43:08 +0200

    Added uniqueness constraint to User schema

--- lib/carmine_gql/accounts/user.ex
+++ lib/carmine_gql/accounts/user.ex
@@ -17,6 +17,7 @@ defmodule CarmineGql.Accounts.User do
     |> cast(attrs, [:name, :email])
     |> validate_required([:name, :email])
     |> cast_assoc(:preferences)
+    |> unique_constraint(:email)
   end
 
   def with_preferences(), do: from(u in User, join: p in assoc(u, :preferences))
